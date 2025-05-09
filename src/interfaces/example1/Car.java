14ad8c9745de1c0c5c9d2e472827f310440554e2
MalicknND <ndiayemalick062@gmail.com>
2025-05-09 14:20:28 +0200

    Overview of interfaces in Java

---
+++ src/interfaces/example1/Car.java
@@ -0,0 +1,15 @@
+package interfaces.example1;
+
+/*Interface is a blueprint for a class.
+It defines a set of methods that a class must implement, but it does not provide any implementation.
+An interface acts as a contract between the class and the outside world.
+It defines what the class is capable of doing, but not how it does it.*/
+
+public interface Car {
+
+    void startEngine();
+
+    void accelerate();
+
+    void brake();
+}

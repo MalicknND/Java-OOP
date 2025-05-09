88cb97ba700d92ed073591a97c010269c3fd39b7
MalicknND <ndiayemalick062@gmail.com>
2025-05-09 15:13:28 +0200

    implementing many interfaces

---
+++ src/interfaces/example3/Main.java
@@ -0,0 +1,10 @@
+package interfaces.example3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.draw();
+        rectangle.move();
+    }
+}
